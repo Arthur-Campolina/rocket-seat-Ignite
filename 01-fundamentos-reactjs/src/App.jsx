dc27fdef7061f37f58987d62b32a2c6b65fe054f
Arthur Campolina <arthurcampolina@hotmail.com>
2022-07-08 16:58:14 -0300

    map + date (lib date-fns)

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -5,21 +5,60 @@ import Post from "./components/post";
 import Sidebar from "./components/sidebar";
 import Counter from "./components/counter";
 
-function App() {
-  const post = {
-    author: "Arthur Campolina",
-    description: "loren",
-  };
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/arthur-campolina.png",
+      name: "Arthur Campolina",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare/" },
+    ],
+    publishedAt: new Date("2022-05-03"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/lutchen1.png",
+      name: "Lutchen Henrique",
+      role: "Desenvolvedor Protheus",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare/" },
+    ],
+    publishedAt: new Date("2022-06-10"),
+  },
+];
 
+function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post post={post} />
-          <Post post={post} />
-          <Post post={post} />
+          {posts.map((post) => (
+            <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+              key={post.author.name}
+            />
+          ))}
         </main>
         <Counter />
       </div>
--- 01-fundamentos-reactjs/src/components/post/index.jsx
+++ 01-fundamentos-reactjs/src/components/post/index.jsx
@@ -1,37 +1,50 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import Avatar from "../avatar";
 import Comment from "../comment";
 import styles from "./Post.module.css";
 
-const Post = (props) => {
+const Post = ({ author, content, publishedAt }) => {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  );
+  const publisedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/arthur-campolina.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Arthur Campolina</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="02/07/2022" dateTime="2022-07-02">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publisedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          {" "}
-          👉 <a href="www">jane.design/doctorcare/</a>
-        </p>
-        <p>
-          <a href="www">#novoprojeto</a> <a href="www">#nlw</a>{" "}
-          <a href="www">#rocketseat</a>
-        </p>
+        {content.map((line) => {
+          if (content.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (content.type === "link") {
+            return (
+              <p>
+                <a href="www">{line.link}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
